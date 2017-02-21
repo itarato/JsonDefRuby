7df9074223bf9b8e5610048005468bb873204de6
Peter Arato <it.arato@gmail.com>
2017-02-21 12:20:30 +0800

    Add files

---
+++ main.rb
@@ -0,0 +1,128 @@
+require 'json'
+
+module JsonDef
+
+  def JsonDef.verify(obj, rule)
+    case rule.class.to_s
+    when 'JsonRuleObject'
+      JsonDef.verify_object(obj, rule)
+    else
+      raise 'Unknown rule at [verify] level'
+    end
+  end
+
+  def JsonDef.verify_value_type(value, type)
+    case type
+    when :object
+      value.kind_of?(Hash)
+    when :number
+      value.kind_of?(Numeric)
+    when :string
+      value.kind_of?(String)
+    when :wildcard
+      # Land of satisfaction.
+      true
+    else
+      raise 'Unknown value type declaration'
+    end
+  end
+
+  def JsonDef.verify_object(obj, rule)
+    return false unless obj.kind_of?(Hash)
+
+    rule.key_rules.each do |key_rule|
+      return false if key_rule.required && !obj.has_key?(key_rule.key)
+      return true unless obj.has_key?(key_rule.key)
+
+      case
+      when key_rule.value.kind_of?(Symbol)
+        return false unless JsonDef.verify_value_type(obj[key_rule.key], key_rule.value)
+
+      when key_rule.value.kind_of?(JsonRuleObject)
+        return false unless JsonDef.verify_object(obj[key_rule.key], key_rule.value)
+
+      else
+        # Expected an exact value given.
+        return false unless obj[key_rule.key] == key_rule.value
+      end
+    end
+
+    # @todo In case of strict, check if there are other not-allowed keys.
+    if rule.strict?
+      obj.each { |k, v| return false unless rule.keys.member?(k) }
+    end
+
+    true
+  end
+
+end
+
+class JsonRuleObject
+
+  attr_reader :key_rules, :keys
+
+  def initialize()
+    @strict = false
+    @key_rules = []
+    @keys = []
+  end
+
+  def strict?
+    @strict
+  end
+
+  def set_strict
+    @strict = true
+    self
+  end
+
+  def add_key_rule(rule)
+    @key_rules.push(rule)
+    @keys.push(rule.key)
+    self
+  end
+
+end
+
+class JsonRuleObjectKey
+
+  attr_reader :required, :key, :value
+
+  def initialize(key)
+    @key = key
+    @required = true
+    @value = :wildcard
+  end
+
+  def set_optional()
+    @required = false
+    self
+  end
+
+  def set_value_type(type)
+    @value = type
+    self
+  end
+
+  def set_value_rule(rule)
+    @value = rule
+    self
+  end
+
+end
+
+obj_rule = JsonRuleObject.new
+  .set_strict
+  .add_key_rule(JsonRuleObjectKey.new('foo').set_value_type(:string))
+
+[
+  '{"foo": "bar"}',
+  '{"foo": "bar", "baz": 12}',
+  '["foo", "bar"]',
+  '{}',
+  '{"bar": "foo"}',
+  '{"foo": 12}'
+].each do |raw|
+  json_obj = JSON.parse(raw);
+  p json_obj, JsonDef.verify(json_obj, obj_rule)
+end
