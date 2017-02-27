84173d3522c655cbcfa66e4350c6ca682b771ba5
Peter Arato <it.arato@gmail.com>
2017-02-27 14:04:53 +0800

    Small conventional cleaning.

--- lib/jsondef.rb
+++ lib/jsondef.rb
@@ -1,3 +1,5 @@
 require_relative 'verify.rb'
 require_relative 'config_reader.rb'
 require_relative 'rules.rb'
+
+# @todo make proper module namespaces
--- lib/verify.rb
+++ lib/verify.rb
@@ -1,11 +1,17 @@
 require_relative 'rules.rb'
 
+# Module that provides JSON validation
+#
+# :call-seq:
+#   json = JSON.parse(json_string)
+#   conf = ConfigReaderFactory.fromYamlFile(config_file_name)
+#   JsonDef.verify(json, conf.rule)
 module JsonDef
 
   ANY_TYPE = :wildcard
   ANY_VALUE = :any_value
 
-  def JsonDef.verify(obj, rule)
+  def self.verify(obj, rule)
     case rule.class.to_s
     when 'JsonRuleObject'
       JsonDef.verify_object(obj, rule)
@@ -24,60 +30,60 @@ module JsonDef
     end
   end
 
-  def JsonDef.verify_single_value(obj, rule)
+  def self.verify_single_value(obj, rule)
     rule.value == JsonDef::ANY_VALUE || rule.value == obj
   end
 
-  def JsonDef.verify_string(obj, rule)
-    return false unless obj.kind_of?(String)
+  def self.verify_string(obj, rule)
+    return false unless obj.is_a?(String)
     return false unless JsonDef.verify_single_value(obj, rule)
     true
   end
 
-  def JsonDef.verify_number(obj, rule)
-    return false unless obj.kind_of?(Numeric)
+  def self.verify_number(obj, rule)
+    return false unless obj.is_a?(Numeric)
     return false unless JsonDef.verify_single_value(obj, rule)
     true
   end
 
-  def JsonDef.verify_bool(obj, rule)
+  def self.verify_bool(obj, rule)
     return false unless obj == true || obj == false
     return false unless JsonDef.verify_single_value(obj, rule)
     true
   end
 
-  def JsonDef.verify_null(obj, rule)
+  def self.verify_null(obj, rule)
     return false unless JsonDef.verify_single_value(obj, rule)
     true
   end
 
-  def JsonDef.verify_object(obj, rule)
-    return false unless obj.kind_of?(Hash)
+  def self.verify_object(obj, rule)
+    return false unless obj.is_a?(Hash)
 
     rule.key_rules.each do |key_rule|
-      return false if key_rule.required && !obj.has_key?(key_rule.key)
-      return true unless obj.has_key?(key_rule.key)
+      return false if key_rule.required && !obj.key?(key_rule.key)
+      return true unless obj.key?(key_rule.key)
 
-      case
-      when key_rule.rule == JsonDef::ANY_TYPE
-        next
-      when key_rule.rule.kind_of?(JsonRuleBase)
+      next if key_rule.rule == JsonDef::ANY_TYPE
+      
+      if key_rule.rule.is_a?(JsonRuleBase)
         return false unless JsonDef.verify(obj[key_rule.key], key_rule.rule)
       else
         return false
       end
     end
 
-    # @todo In case of not allowing other keys, check if there are other not-allowed keys.
-    if !rule.allow_other_keys
-      obj.each { |k, v| return false unless rule.keys.member?(k) }
+    # @todo In case of not allowing other keys, check if there are other
+    # not-allowed keys.
+    unless rule.allow_other_keys
+      obj.each { |k, _| return false unless rule.keys.member?(k) }
     end
 
     true
   end
 
-  def JsonDef.verify_array(obj, rule)
-    return false unless obj.kind_of?(Array)
+  def self.verify_array(obj, rule)
+    return false unless obj.is_a?(Array)
     return false unless rule.min_count < 0 || obj.count >= rule.min_count
     return false unless rule.max_count < 0 || obj.count <= rule.max_count
 
